4c0a20990496d6a67089f3868617fd58aadce1a0
weiwei <41343149@nfu.edu.tw>
2025-12-21 19:18:58 +0800

    Update main.cpp

--- homework3/src/main.cpp
+++ homework3/src/main.cpp
@@ -1,1 +1,15 @@
+// main.cpp
+int main() {
+    Polynomial A, B;
+    double x;
 
+    cin >> A >> B >> x;
+
+    cout << "A = " << A << "\n";
+    cout << "B = " << B << "\n";
+    cout << "A + B = " << (A + B) << "\n";
+    cout << "A - B = " << (A - B) << "\n";
+    cout << "A * B = " << (A * B) << "\n";
+    cout << "A(" << x << ") = " << A.eval(x) << "\n";
+    return 0;
+}
