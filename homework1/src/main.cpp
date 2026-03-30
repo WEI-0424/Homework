ada5e85665088808a1d8158feceeec5ec3e13257
weiwei <41343149@nfu.edu.tw>
2026-03-31 00:21:16 +0800

    Update main.cpp

--- homework1/src/main.cpp
+++ homework1/src/main.cpp
@@ -1,1 +1,21 @@
+int main() {
+    MinHeap<int> h;
 
+    h.Push(12);
+    h.Push(6);
+    h.Push(8);
+    h.Push(7);
+    h.Push(15);
+    h.Push(3);
+
+    cout << "Min Heap: ";
+    h.PrintHeap();
+
+    cout << "Top = " << h.Top() << endl;
+
+    h.Pop();
+    cout << "After Pop: ";
+    h.PrintHeap();
+
+    return 0;
+}
